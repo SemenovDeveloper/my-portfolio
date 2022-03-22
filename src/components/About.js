c51196f2fa4f3c035cce9f91c527101d576bcfc0
Alexander Semenov <SemenovDev@yahoo.com>
2022-03-22 13:48:35 +0600

    create base

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div id="about">
+      <div id="about-me">
+        <h3>I'm Alexander Semenov.</h3>
+        <p>I'm a <b>Frontiend Web Developer</b>. I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+      </div>
+      <div id="image-block"><img alt="My photo"></img></div>
+    </div>  
+  )
+}
+
+export default About
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar () {
+  return(
+    <div id="nav-bar">
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
